875b2899a8c672f5bf3df2c704bb3e7198e52a89
Shane Panter <spanter@cs.boisestate.edu>
2016-10-19 15:26:32 -0600

    Added in some stub files

---
+++ lecture/CS117/Cipher.cpp
@@ -0,0 +1,5 @@
+//
+// Created by spanter on 11/3/15.
+//
+
+#include "Cipher.h"
---
+++ lecture/CS117/Cipher.h
@@ -0,0 +1,14 @@
+//
+// Created by spanter on 11/3/15.
+//
+
+#ifndef CS117_CIPHER_H
+#define CS117_CIPHER_H
+
+
+class Cipher {
+
+};
+
+
+#endif //CS117_CIPHER_H
