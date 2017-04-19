ebceed9e00c7019afb0a0152fb72da38353759dd
ddthompson <davidthompsonhere@gmail.com>
2017-04-19 07:57:55 -0600

    Create Assembly.h

---
+++ MiscProjects/Assembly.h
@@ -0,0 +1,22 @@
+//Assembly.h
+#ifndef ASSEMBLY_H
+#define ASSEMBLY_H
+#include <vector>
+#include "Part.h"
+
+using namespace std;
+
+class Assembly : public Part{
+private:
+    vector<Part> m_parts;
+public:
+    //Assembly constructor
+    Assembly(string id, string dwg, int qty);
+    Assembly();
+    //Add a part to this assembly
+    void addPart(Part p);
+    //Print out the BOM
+    string toString();
+};
+#endif
+
