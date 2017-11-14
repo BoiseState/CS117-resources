7bc4da89560a9d0feac04494a017958a7e751146
ddthompson <davidthompsonhere@gmail.com>
2017-11-13 18:42:58 -0700

    Update Rectangle.h

--- Chapter10_Inheritance/RectangleSquare/Rectangle.h
+++ Chapter10_Inheritance/RectangleSquare/Rectangle.h
@@ -6,6 +6,8 @@
 #define RECTSQUARE_RECTANGLE_H
 
 class Rectangle {
+
+
 public:
     Rectangle(double h, double l);
 
@@ -17,18 +19,22 @@ public:
     double getPerimeter() const;
     double getArea() const;
     void printIt() const;
+    std::string getColor() const;
 
 private:
     //
-    // setArea and setPerimeter are 'private'.  This will NOT work for 
+    // setColor is 'private'.  This will NOT work for
     // derived classes, so what needs to change?
     //
     void setArea(double h, double l);
     void setPerimeter(double h, double l);
+    void setColor(std::string color);
+
 
     double height, length;
     double area;
     double perimeter;
+    std::string color;
 
 };
 
