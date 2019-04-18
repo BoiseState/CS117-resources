5806dcc84f0e0b4722912217d8aa0e876bcc0d07
ddthompson <davidthompsonhere@gmail.com>
2019-04-17 20:23:02 -0600

    Update MilitaryMember.cpp

--- Chapter10_Inheritance/MilitaryMember.cpp
+++ Chapter10_Inheritance/MilitaryMember.cpp
@@ -5,6 +5,14 @@
 #include <iostream>
 #include "MilitaryMember.h"
 
+
+MilitaryMember::MilitaryMember(string branch, string rank,
+                               string fname, string lname, char sex, string ssNum) : Person(fname, lname, sex, ssNum)
+{
+    this->branch = branch;
+    this->rank = rank;
+}
+
 void MilitaryMember::setBranch(string name) {
     this->branch = name;
 }
@@ -14,7 +22,7 @@ void MilitaryMember::setRank(string rank) {
 }
 
 void MilitaryMember::printServiceMember() const {
-    printPerson();
+    Person::printPerson();
     cout << "Branch: " << this->branch << endl;
     cout << "Rank:   " << this->rank << endl;
 }
