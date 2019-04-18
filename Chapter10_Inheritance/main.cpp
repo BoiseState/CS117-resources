4976f10254b735507d6d0ca9661122d42894b151
ddthompson <davidthompsonhere@gmail.com>
2019-04-17 20:27:14 -0600

    Update main.cpp

--- Chapter10_Inheritance/main.cpp
+++ Chapter10_Inheritance/main.cpp
@@ -1,5 +1,7 @@
+// InheritanceExample.cpp : Defines the entry point for the console application.
+//
+
 #include <iostream>
-#include <stdlib.h>
 #include "MilitaryMember.h"
 #include "Student.h"
 
@@ -7,22 +9,33 @@ using namespace std;
 
 int main() {
 
-    MilitaryMember mm1;
+    Person person ("david","thompson",'M',"123456789");
+
+    //
+    // Use the constructors of the base class and the derived class to create the Military Member
+    // object...
+    //
+    MilitaryMember mm1("Air Force", "Private", "John", "Military", 'M', "123-45-6789");
+    //
+    // Use the default constructor of the student class And the default constructor of the base
+    // class to create the student object.
+    //
     Student s1;
-    mm1.setFname("John");
-    mm1.setLname("Military");
-    mm1.setSex('M');
-    mm1.set_ssNum("123-456-7890");
-    mm1.setRank("Private");
-    mm1.setBranch("Air Force");
-    mm1.printServiceMember();
-    cout << endl << endl;
+    //
+    // Play with student class
+    //
     s1.setFname("Mary");
     s1.setLname("Student");
     s1.setSex('F');
-    s1.set_ssNum("213-645-0987");
+    s1.set_ssNum("213-65-0987");
     s1.setMajor("Electrical Engineering");
     s1.setStudentID(12345);
     s1.printPerson();
+
+    //
+    // print out the military member object.
+    //
+    mm1.printServiceMember();
+    mm1.printPerson();
     return 0;
 }
